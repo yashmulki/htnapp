eb6a7a074735c279fb2ef6ea92a4aced5fc3033e
Yashvardhan Mulki <yash.mulki@hotmail.com>
2021-01-17 00:15:46 -0500

    no clue what im doing

---
+++ htnappui/GameResultScreen.swift
@@ -0,0 +1,20 @@
+//
+//  GameResultScreen.swift
+//  htnappui
+//
+//  Created by Yashvardhan Mulki on 2021-01-16.
+//
+
+import SwiftUI
+
+struct GameResultScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameResultScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        GameResultScreen()
+    }
+}
